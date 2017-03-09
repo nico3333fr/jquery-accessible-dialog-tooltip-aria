5f7b90388b75766e2a98032094ec538b8735fe35
Nicolas <dante3333@gmail.com>
2017-03-09 13:59:19 +0100

    Update URL to https

--- jquery-accessible-dialog-tooltip-aria.js
+++ jquery-accessible-dialog-tooltip-aria.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
    /*
     * jQuery simple and accessible dialog tooltip window, using ARIA
     * @version v1.1.2
-    * Website: http://a11y.nicolas-hoffmann.net/dialog-tooltip/
+    * Website: https://a11y.nicolas-hoffmann.net/dialog-tooltip/
     * License MIT: https://github.com/nico3333fr/jquery-accessible-dialog-tooltip-aria/blob/master/LICENSE
     */
    // loading tooltip ------------------------------------------------------------------------------------------------------------
